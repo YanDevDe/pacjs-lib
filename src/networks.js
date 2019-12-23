a900174c9d1566e059c801cb7463f9bbd9066ec4
Chinafreak <yan@huelsebusch.com>
2019-12-23 15:10:24 +0100

    first commit

---
+++ src/networks.js
@@ -0,0 +1,13 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.bitcoin = { //PAC stats
+  messagePrefix: '\x19DarkCoin Signed Message:\n',
+  bech32: '',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4,
+  },
+  pubKeyHash: 0x37,
+  scriptHash: 0x0a,
+  wif: 0xcc,
+};
